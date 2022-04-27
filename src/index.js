6cc844cfaa0e34885b7b4c2a18b9431bce81a7c8
Patrick <patrickmcalinden226@gmail.com>
2022-04-27 00:24:59 -0400

    test

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
+root.render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+    </BrowserRouter>)
