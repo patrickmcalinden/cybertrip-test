9de4d407a964ada84772e34e75d278ee9e19af0b
Patrick <patrickmcalinden226@gmail.com>
2022-04-29 19:52:31 -0400

    test

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Register from '../components/Register'
+import ScrollToTop from '../components/ScrollToTop'
 
 const RegisterPage = () => {
   return (
       <>
+        <ScrollToTop />
         <Register />
       </>
 
