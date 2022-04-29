95b2e25ae5acdfc11e26e20aa4f099ed76a0f731
Patrick <patrickmcalinden226@gmail.com>
2022-04-29 18:01:48 -0400

    test

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -4,7 +4,7 @@ import {Link as LinkS} from 'react-scroll'
 import {FaTimes} from 'react-icons/fa'
 
 export const SidebarContainer = styled.aside`
-    position: relative;
+    position: fixed;
     z-index: 999;
     width: 100%;
     height: 100%;
@@ -14,7 +14,6 @@ export const SidebarContainer = styled.aside`
     align-items: center;
     top: 0;
     left: 0;
-    overflow: auto;
     transition: .5s ease-in-out;
     opacity: ${( {isOpen} ) => (isOpen ? '100%' : '0')};
     top: ${({ isOpen }) => (isOpen ? '0' : '-100%')};
